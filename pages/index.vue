d2ab0de9889496e2eca99559eb0425fab37d71f2
Wolf Syntax <jaysonalpe@gmail.com>
2023-07-23 07:47:38 +0800

    Integrate tailwindcss

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,11 @@
 <template>
   <div>
     <h1>Home</h1>
+    <div class="grid grid-cols-4 gap-4">
+    <div class="rounded-full bg-gray-dark">01</div>
+    <!-- ... -->
+    <div>09</div>
+  </div>
   </div>
 </template>
 
